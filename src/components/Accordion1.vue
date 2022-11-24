d4b8dbe03b36f83440f4621adbf008bc73a7a269
unknown <shahedsaiem.jatri@gmail.com>
2022-11-24 11:50:35 +0600

    accordion updated

---
+++ src/components/Accordion1.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="border-b border-b-[#DBDBDB] last:border-b-0 py-4">
+    <button @click="setAccordionStatus()" class="flex justify-between items-center w-full focus:outline-0 outline-0">
+      <h2 class="text-base text-blackPrimary font-medium leading-6 text-left">{{heading}}</h2>
+      <img src="../assets/images/svg/down-arrow.svg" alt="" :class="open && (selectedIndex === uniqeId) ? 'transform rotate-180 transition-all duration-300 ease-in-out': ''">
+    </button>
+    <div :class="open && (selectedIndex === uniqeId) ? 'max-h-max transition-all duration-300 ease-in-out': 'max-h-0 overflow-hidden'">
+      <p class="text-sm text-blackLight font-normal py-4 text-justify" v-html="details"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['heading', 'details', 'uniqeId', 'selectedIndex'],
+  data(){
+    return{
+      open: false,
+    }
+  },
+  methods: {
+    setAccordionStatus(){
+      this.open = !this.open
+      this.$emit('setAccordionIndex', this.uniqeId)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -4,7 +4,17 @@
 <div class="mt-20 lg:mt-0">
 <Slider/>
 <div class="mt-5 lg:mt-0">
-<Accordion/>
+  <div class="w-full mt-6 lg:mt-20">
+    <Accordion1
+        v-for="item in items"
+        :heading="item.details"
+        :details="item.title"
+        :uniqeId="item.id"
+        :selectedIndex="selectedAccordion"
+        @setAccordionIndex="setAccordionIndex"
+    />
+
+  </div>
 </div>
 <div class="mt-5 lg:mt-0">
 <Tips/>
@@ -16,10 +26,38 @@
 <script>
 import Banner from '../components/Banner.vue'
 import Slider from '../components/Slider.vue'
-import Accordion from '../components/Accordion.vue'
+import Accordion1 from '../components/Accordion1.vue'
 import Tips from '../components/Tips.vue'
 export default{
     name:'Home',
-    components:{Banner,Slider,Accordion,Tips}
+    components:{Banner,Slider,Accordion1,Tips},
+  data(){
+      return{
+        selectedAccordion:null,
+        items:[
+          {
+            title:' It was a pleasant evening at The Society Achievers Awards. Our founder Mr. Jagadeesh (Jai) Yelimanchili our ambassa… ',
+            details:'haha',
+            id:'1'
+          },
+          {
+            title:' It was a pleasant evening at The Society Achievers Awards. Our founder Mr. Jagadeesh (Jai) Yelimanchili our ambassa… ',
+            details:'haha two',
+            id:'2'
+          },
+          {
+            title:' It was a pleasant evening at The Society Achievers Awards. Our founder Mr. Jagadeesh (Jai) Yelimanchili our ambassa… ',
+            details:'haha three',
+            id:'3'
+          }
+        ]
+      }
+  },
+
+  methods:{
+    setAccordionIndex(id){
+      this.selectedAccordion = id;
+    },
+  }
 }
 </script>
