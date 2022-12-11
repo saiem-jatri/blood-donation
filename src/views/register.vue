c21e0774150f1a45efd90658492459c00d54b5ed
unknown <shahedsaiem.jatri@gmail.com>
2022-12-11 17:53:52 +0600

    blood section added

--- src/views/register.vue
+++ src/views/register.vue
@@ -3,7 +3,6 @@
         <div class="bg-purple-600 min-h-screen flex items-center text-lg">
    <form action="" class="p-10 md:w-2/3 lg:w-1/2 mx-auto rounded">
      <div class="shadow">
-        {{donarName.val}}
        <div class="flex items-center bg-purple-400 rounded-t-lg border-purple-500 border-b mb-5">
          <label for="name" class="w-40 text-right mr-8 p-4 text-purple-200">Donar Name</label>
          <input v-model.trim="donarName.val" type="text" name="name" id="name" placeholder="Put in your name" class="flex-1 p-4 pl-0 bg-transparent placeholder-purple-300  outline-none text-white overflow-ellipsis overflow-hidden">
@@ -13,6 +12,13 @@
          <label for="twitter" class="w-40 text-right p-4 mr-8 text-purple-200">Donar Age</label>
          <input v-model.trim="donarAage.val" type="text" name="twitter" id="age" placeholder="Put your age here" class="flex-1 p-4 pl-0 bg-transparent placeholder-purple-300 outline-none text-white overflow-ellipsis overflow-hidden">
        </div>
+
+       <p v-if="donarName.isValid" class="text-red-700">Please input your Blood Group!!</p>
+       <div class="flex items-center bg-purple-400  rounded-b-lg border-purple-500 mb-5">
+         <label for="twitter" class="w-40 text-right p-4 mr-8 text-purple-200">Blood Group</label>
+         <input v-model.trim="bloodGroup.val" type="text" name="twitter" id="age" placeholder="Put your Blood Group" class="flex-1 p-4 pl-0 bg-transparent placeholder-purple-300 outline-none text-white overflow-ellipsis overflow-hidden">
+       </div>
+
        <p v-if="donarAage.isValid" class="text-red-700">Please input your age!!</p>
        <div class="flex items-center bg-purple-400  rounded-b-lg border-purple-500 mb-5">
          <label for="twitter" class="w-40 text-right p-4 mr-8 text-purple-200">Donar phone</label>
@@ -72,6 +78,10 @@ import {ref} from "vue";
         val: '',
         isValid: false,
     })
+const bloodGroup = reactive({
+        val: '',
+        isValid: false,
+    })
 const formIsValid = ref(true)
 
 
@@ -104,9 +114,13 @@ const addBlogsData = () => {
     totalDotation.isValid = true;
     formIsValid.value = false;
   }
+  if (bloodGroup.val === '') {
+    bloodGroup.isValid = true;
+    formIsValid.value = false;
+  }
 else {
     console.log("name",donarName.val)
-    const payloads = {'donarName':donarName.val, 'donarAge':donarAage.val,'donarInfo':donarInfo.val,'donarId':donarId.val,'donarphone':donarPhone.val,'totalDonateTime':totalDotation.val}
+    const payloads = {'donarName':donarName.val, 'bloodGroup':bloodGroup.val, 'donarAge':donarAage.val,'donarInfo':donarInfo.val,'donarId':donarId.val,'donarphone':donarPhone.val,'totalDonateTime':totalDotation.val}
     console.log("payloads",payloads)
     store.dispatch('json/addBlogsData',payloads)
   }
